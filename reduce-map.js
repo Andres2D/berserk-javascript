111a3a843b11f132fe091a3d0cce03a8d5d5349a
Andres2D <andres2d1997.1206@gmail.com>
2022-07-12 16:47:56 -0500

    Add custom map reducer

--- reduce-map.js
+++ reduce-map.js
@@ -10,7 +10,25 @@ const newArray = baseArray.map((val) => {
   return val * 2;
 });
 
-// console.log(newArray);
+console.log(`From original map [${newArray}]`);
 
+/**
+ * Now, if we need to generate our own map function 
+ * will be something like this:
+ */
+ Array.prototype.customMap = function(callback, ctx = null){
+  if(typeof callback !== 'function'){
+    throw('The callback must be a function');
+  }
+  
+  return this.reduce((result, current, idx, arr) => {
+    return result.concat(callback.call(ctx, current, idx, arr));
+  },[]);
+  
 
+};
 
+const newCustomArray = baseArray.customMap((val, idx, arr) => {
+  return val * 2;
+});
+console.log(`From custom map [${newCustomArray}]`);
